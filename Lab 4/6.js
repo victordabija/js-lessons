13dd9de0e3684d7eaf180cce215b4177e7729b2a
victor.dabija <victor.dabija@bpay.md>
2024-01-25 10:25:15 +0200

    fix: Lab 4, ex 6

--- Lab 4/6.js
+++ Lab 4/6.js
@@ -9,7 +9,27 @@ const products = {
     5: 6.87
 }
 
-if (productNumber in products && cantitateaProdusului >= 1) {
+let suma;
+
+switch (productNumber) {
+    case 1:
+        suma = cantitateaProdusului * 2.98;
+        break;
+    case 2:
+        suma = cantitateaProdusului * 4.5;
+        break;
+    case 3:
+        suma = cantitateaProdusului * 9.98;
+        break;
+    case 4:
+        suma = cantitateaProdusului * 4.49;
+        break;
+    case 5:
+        suma = cantitateaProdusului * 6.87;
+        break;
+}
+
+if (suma) {
     alert(`Suma: ${cantitateaProdusului * products[productNumber]} $`);
 } else {
     alert("Error");
