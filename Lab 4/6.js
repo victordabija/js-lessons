7a0b2e9c5f2d12f0812926f8958951f55ecdc926
victor.dabija <victor.dabija@bpay.md>
2024-01-29 10:01:45 +0200

    fix: Lab 4 ex 6

--- Lab 4/6.js
+++ Lab 4/6.js
@@ -1,24 +1,32 @@
-const productNumber = parseInt(prompt("Introduceti numarul produsului: "));
-const cantitateaProdusului = parseInt(prompt("Introduceti cantitatea: "));
-
+let productNumber = true;
 let suma;
 
-switch (productNumber) {
-    case 1:
-        suma = cantitateaProdusului * 2.98;
+while (productNumber) {
+    productNumber = parseInt(prompt("Introduceti numarul produsului: "));
+
+    if (!productNumber) {
         break;
-    case 2:
-        suma = cantitateaProdusului * 4.5;
-        break;
-    case 3:
-        suma = cantitateaProdusului * 9.98;
-        break;
-    case 4:
-        suma = cantitateaProdusului * 4.49;
-        break;
-    case 5:
-        suma = cantitateaProdusului * 6.87;
-        break;
+    }
+
+    const cantitateaProdusului = parseInt(prompt("Introduceti cantitatea: "));
+
+    switch (productNumber) {
+        case 1:
+            suma = cantitateaProdusului * 2.98;
+            break;
+        case 2:
+            suma = cantitateaProdusului * 4.5;
+            break;
+        case 3:
+            suma = cantitateaProdusului * 9.98;
+            break;
+        case 4:
+            suma = cantitateaProdusului * 4.49;
+            break;
+        case 5:
+            suma = cantitateaProdusului * 6.87;
+            break;
+    }
 }
 
 if (suma) {
